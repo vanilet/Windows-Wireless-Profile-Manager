f3a5081fc76cd520b9ae2cac27a9bf72bcd31d41
CHEOLMIN KIM <vanilet@vanilet.pe.kr>
2015-04-29 14:10:16 +0900

    - First commit.  - Added referenced project <Managed Wifi API (v1.1)>  - Added GUI components to lookup SSID and it's variables.

---
+++ WirelessProfileManager/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WirelessProfileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxInterface = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView_ScannedBssList = new System.Windows.Forms.DataGridView();
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_ScannedBssList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Interfaces";
+            // 
+            // cbxInterface
+            // 
+            this.cbxInterface.FormattingEnabled = true;
+            this.cbxInterface.Location = new System.Drawing.Point(15, 34);
+            this.cbxInterface.Name = "cbxInterface";
+            this.cbxInterface.Size = new System.Drawing.Size(370, 26);
+            this.cbxInterface.TabIndex = 1;
+            this.cbxInterface.SelectedIndexChanged += new System.EventHandler(this.cbxInterface_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Scanned SSID list";
+            // 
+            // dataGridView_ScannedBssList
+            // 
+            this.dataGridView_ScannedBssList.AllowUserToAddRows = false;
+            this.dataGridView_ScannedBssList.AllowUserToDeleteRows = false;
+            this.dataGridView_ScannedBssList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_ScannedBssList.Location = new System.Drawing.Point(15, 97);
+            this.dataGridView_ScannedBssList.MultiSelect = false;
+            this.dataGridView_ScannedBssList.Name = "dataGridView_ScannedBssList";
+            this.dataGridView_ScannedBssList.ReadOnly = true;
+            this.dataGridView_ScannedBssList.RowTemplate.Height = 30;
+            this.dataGridView_ScannedBssList.Size = new System.Drawing.Size(749, 335);
+            this.dataGridView_ScannedBssList.TabIndex = 3;
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(WirelessProfileManager.Form1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 444);
+            this.Controls.Add(this.dataGridView_ScannedBssList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxInterface);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "WirelessProfileManager";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_ScannedBssList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxInterface;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView_ScannedBssList;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+    }
+}
+
---
+++ WirelessProfileManager/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NativeWifi;
+
+namespace WirelessProfileManager
+{
+    public partial class Form1 : Form
+    {
+        #region Field
+        private WlanClient wlanClient;
+
+        private List<String> interfaceList;
+        #endregion Field
+
+        #region Constant
+        private const String KEY_SSID = "SSID";
+        private const String KEY_PHY_TYPE = "PHY_TYPE";
+        private const String KEY_RSSI = "RSSI";
+        private const String KEY_BSSID = "BSSID";
+        #endregion Constant
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            setWaitCursor();
+
+            wlanClient = new WlanClient();
+
+            interfaceList = new List<String>();
+
+            dataGridView_ScannedBssList.ColumnCount = 4;
+            dataGridView_ScannedBssList.Columns[0].Name = KEY_SSID;
+            dataGridView_ScannedBssList.Columns[1].Name = KEY_PHY_TYPE;
+            dataGridView_ScannedBssList.Columns[2].Name = KEY_BSSID;
+            dataGridView_ScannedBssList.Columns[3].Name = KEY_RSSI;
+
+            initInterfaces();
+
+            setDefaultCursor();
+        }
+
+        private void initInterfaces()
+        {
+            foreach(var item in wlanClient.Interfaces)
+                interfaceList.Add(item.InterfaceName);
+
+            cbxInterface.DataSource = interfaceList;
+        }
+
+        private void setWaitCursor()
+        {
+            UseWaitCursor = true;
+
+            Cursor = Cursors.WaitCursor;
+        }
+
+        private void setDefaultCursor()
+        {
+            UseWaitCursor = false;
+
+            Cursor = Cursors.Default;
+        }
+
+        private void cbxInterface_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(interfaceList.Count() > 0)
+            {
+                setWaitCursor();
+
+                var currentInterface = wlanClient.Interfaces[cbxInterface.SelectedIndex];
+                
+                currentInterface.Scan();
+
+                dataGridView_ScannedBssList.Rows.Clear();
+
+                foreach(var item in currentInterface.GetNetworkBssList())
+                {
+                    String[] entity = {
+                                          System.Text.Encoding.UTF8.GetString(item.dot11Ssid.SSID),
+                                          String.Format("802.11{0}", item.dot11BssPhyType.ToString()),
+                                          String.Format("{0:x}:{1:x}:{2:x}:{3:x}:{4:x}:{5:x}",
+                                                item.dot11Bssid[0], item.dot11Bssid[1],item.dot11Bssid[2],
+                                                item.dot11Bssid[3], item.dot11Bssid[4], item.dot11Bssid[5]),
+                                          String.Format("{0}dbm", item.rssi.ToString())
+                                      };
+
+                    dataGridView_ScannedBssList.Rows.Add(entity);
+                }
+
+                dataGridView_ScannedBssList.Update();
+
+                setDefaultCursor();
+            }
+        }
+    }
+}
